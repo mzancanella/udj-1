34fcd64c18415dc0954d82d0e0ff7b7a6530a457
Matthew Brock <mtbrock1@gmail.com>
2013-10-09 19:04:47 -0400

    got header template working

--- www/js/views/header.js
+++ www/js/views/header.js
@@ -1,10 +1,17 @@
 window.HeaderView = Backbone.View.extend({
 
-	initialize: function () {
-	},
+    initialize: function () {
 
-	render: function () {
-		$(this.el).html(this.template());
-		return this;
-	}
+    },
+
+    render: function () {
+        $(this.el).html(this.template());
+        return this;
+    },
+
+    select: function(menuItem) {
+        $('.nav li').removeClass('active');
+        $('.' + menuItem).addClass('active');
+    }
+
 });
