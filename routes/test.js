345165c31a9258a89e2f2c8baa2722b7d95c654b
hjkang57 <hjkang57@gmail.com>
2013-10-10 19:55:53 -0400

    database, mongo

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,13 @@
-exports.hello = function(req, res) {
-    res.send('hello');
-};
-
-exports.goodbye = function(reg, res) {
-    res.send('See ya!');
-};
+var mongo = require('mongodb');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db
+ 
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('udjDB', server);
+ 
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'udjDB' database");
+    }
+});
